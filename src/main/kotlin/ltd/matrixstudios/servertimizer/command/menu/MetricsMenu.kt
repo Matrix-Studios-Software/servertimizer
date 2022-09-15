31c327cec406a9b5de1af4544546c0f9cc5c012d
Max <67809373+98ping@users.noreply.github.com>
2022-09-14 18:07:39 -0700

    add frozen server entry button to metrics menu

--- src/main/kotlin/ltd/matrixstudios/servertimizer/command/menu/MetricsMenu.kt
+++ src/main/kotlin/ltd/matrixstudios/servertimizer/command/menu/MetricsMenu.kt
@@ -1,12 +1,24 @@
 package ltd.matrixstudios.servertimizer.command.menu
 
+import ltd.matrixstudios.servertimizer.freeze.menus.FreezeEntryMenu
 import ltd.matrixstudios.servertimizer.util.menu.Button
 import ltd.matrixstudios.servertimizer.util.menu.Menu
+import ltd.matrixstudios.servertimizer.util.menu.buttons.PlaceholderButton
+import ltd.matrixstudios.servertimizer.util.menu.buttons.SimpleActionButton
+import org.bukkit.Material
 import org.bukkit.entity.Player
 
 class MetricsMenu(val player: Player) : Menu(18, player) {
     override fun getButtons(player: Player): MutableMap<Int, Button> {
         val buttons = hashMapOf<Int, Button>()
+        buttons[0] = SimpleActionButton(
+            Material.ICE,
+            mutableListOf(),
+            "&bFrozen Server Entries",
+            0
+        ).setClickAction { player, i, clickType ->
+            FreezeEntryMenu(player).updateMenu()
+        }
 
         return buttons
     }
